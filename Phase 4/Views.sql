d4835423f19d85b94a4a1eb79574e9fbde1f1d25
LakshmanRaaj <senthilnathan.l@northeastern.edu>
2023-04-10 12:09:46 -0400

    Added four views

--- Phase 4/Views.sql
+++ Phase 4/Views.sql
@@ -30,7 +30,7 @@ GROUP BY ty.transaction_type
 ORDER BY ty.transaction_type;
 
 --V_HOURLY_FAILED_TRANSACTION: Fetches all the transactions started in the last
---hour  9PM of 16th March 2023 , which is in a "failure‚Äù status.
+--hour  9PM of 16th March 2023 , which is in a "failure‚Ä? status.
 CREATE OR REPLACE VIEW V_HOURLY_FAILED_TRANSACTION_16th_March AS
     SELECT ty.transaction_type, COUNT(tr.transaction_id) AS num_transactions, SUM(tr.amount) AS total_amount
     FROM transaction_table tr
@@ -41,7 +41,7 @@ CREATE OR REPLACE VIEW V_HOURLY_FAILED_TRANSACTION_16th_March AS
     ORDER BY ty.transaction_type;
 
 --V_HOURLY_FAILED_TRANSACTION: Fetches all the transactions started in the last
---hour  from the current timestamp , which is in a "failure‚Äù status.
+--hour  from the current timestamp , which is in a "failure‚Ä? status.
 --CREATE VIEW V_HOURLY_FAILED_TRANSACTION AS
 CREATE OR REPLACE VIEW V_HOURLY_FAILED_TRANSACTION AS
     SELECT ty.transaction_type, COUNT(tr.transaction_id) AS num_transactions, SUM(tr.amount) AS total_amount
@@ -52,4 +52,53 @@ CREATE OR REPLACE VIEW V_HOURLY_FAILED_TRANSACTION AS
     GROUP BY ty.transaction_type
     ORDER BY ty.transaction_type;
 
+--V_AGG_DAILY_TRANSACTIONS_BY_TYPE: Fetch the aggregate of the transactions
+--started within the last day from 16th March 2023 across all the accounts. The returned
+--set should be grouped by their type
+CREATE OR REPLACE VIEW V_AGG_DAILY_TRANSACTIONS_BY_TYPE_15th_March AS
+    SELECT tr.transaction_type, COUNT(T.transaction_id) AS num_transactions, SUM(T.amount) as total_amount
+    FROM transaction_table T
+             JOIN transaction_type tr ON T.transaction_type = tr.transaction_type_id
+             JOIN accounts A ON T.account_id = A.account_id
+    WHERE TRUNC(T.time_stamp) = TRUNC(TO_DATE('16-MAR-2023', 'DD-MON-YYYY')) - 1
+      AND T.Status != 'Failed'
+    GROUP BY tr.transaction_type
+    ORDER BY tr.transaction_type;
+
+--V_AGG_DAILY_TRANSACTIONS_BY_TYPE: Fetch the aggregate of the transactions
+--started within the last day from current date across all the accounts. The returned
+--set should be grouped by their type
+CREATE OR REPLACE VIEW V_AGG_DAILY_TRANSACTIONS_BY_TYPE As
+    SELECT tr.transaction_type, COUNT(T.transaction_id) AS num_transactions, SUM(T.amount) as total_amount
+    FROM transaction_table T
+             JOIN transaction_type tr ON T.transaction_type = tr.transaction_type_id
+             JOIN accounts A ON T.account_id = A.account_id
+    WHERE TRUNC(T.time_stamp) = TRUNC(SYSDATE) - 1
+      AND T.Status != 'Failed'
+    GROUP BY tr.transaction_type
+    ORDER BY tr.transaction_type;
+
+--V_DAILY_FAILED_TRANSACTIONS: Fetches all the transactions started in the last
+--day from 16th March 2023, which is in a "failureî status.
+CREATE OR REPLACE VIEW V_DAILY_FAILED_TRANSACTIONS_15th_March AS
+    SELECT tr.transaction_type, COUNT(T.transaction_id) AS num_transactions, SUM(T.amount) as total_amount
+    FROM transaction_table T
+             JOIN transaction_type tr ON T.transaction_type = tr.transaction_type_id
+             JOIN accounts A ON T.account_id = A.account_id
+    WHERE TRUNC(T.time_stamp) = TRUNC(TO_DATE('16-MAR-2023', 'DD-MON-YYYY')) - 1
+      AND T.Status = 'Failed'
+    GROUP BY tr.transaction_type
+    ORDER BY tr.transaction_type;
+
+--V_DAILY_FAILED_TRANSACTIONS: Fetches all the transactions started in the last
+--day from current date, which is in a "failureî status.
+CREATE OR REPLACE VIEW V_DAILY_FAILED_TRANSACTIONS AS
+    SELECT tr.transaction_type, COUNT(T.transaction_id) AS num_transactions, SUM(T.amount) as total_amount
+    FROM transaction_table T
+             JOIN transaction_type tr ON T.transaction_type = tr.transaction_type_id
+             JOIN accounts A ON T.account_id = A.account_id
+    WHERE TRUNC(T.time_stamp) = TRUNC(SYSDATE) - 1
+      AND T.Status = 'Failed'
+    GROUP BY tr.transaction_type
+    ORDER BY tr.transaction_type;
     
