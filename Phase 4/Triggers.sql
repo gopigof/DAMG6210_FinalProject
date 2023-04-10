174ec82cfcc53ef4b95ec475e00b9d0be6a2bc2e
LakshmanRaaj <senthilnathan.l@northeastern.edu>
2023-04-10 11:44:54 -0400

    Added two more Triggers

--- Phase 4/Triggers.sql
+++ Phase 4/Triggers.sql
@@ -30,3 +30,34 @@ BEGIN
 
    END IF;
 END;
+
+CREATE OR REPLACE TRIGGER transaction_success_transfer
+AFTER INSERT ON transaction_table
+FOR EACH ROW 
+DECLARE
+   transfer_type CHAR(30);
+BEGIN
+   transfer_type := SUBSTR(:new.transaction_details, 1,11);
+   IF (transfer_type = 'Transfer to') AND (:new.status = 'Completed') AND (:new.status_code = '00') AND (:new.transaction_type = 3) THEN
+      UPDATE accounts
+      SET balance = balance - :new.amount
+      WHERE account_id = :new.account_id;
+      
+      DBMS_OUTPUT.PUT_LINE('Transaction ' || :new.transaction_id || ' was successful.');
+      
+   END IF;
+END;
+
+CREATE OR REPLACE TRIGGER transaction_success_credit
+AFTER INSERT ON transaction_table
+FOR EACH ROW
+BEGIN
+   IF (:new.status = 'Completed') AND (:new.status_code = '00') AND (:new.transaction_type = 1) THEN
+      UPDATE accounts
+      SET balance = balance + :new.amount
+      WHERE account_id = :new.account_id;  
+      
+      DBMS_OUTPUT.PUT_LINE('Transaction ' || :new.transaction_id || ' was successful.');
+      
+   END IF;  
+END;
