9335adbe24944b70041397a5841508fc4dd2fdc7
Gopi Krishna <gopigof@gmail.com>
2023-04-12 11:30:55 -0400

    [Feat] Added DB Indexes on table

--- Phase 4/indexes.sql
+++ Phase 4/indexes.sql
@@ -2,25 +2,21 @@
 -- User Defined Indexes
 --------------------------------------------------------------
 
--- Customer Table Index
-CREATE INDEX idx_customer_login_username ON CUSTOMER (LOGIN);
-
--- Department Table Index
-CREATE INDEX idx_department_location ON DEPARTMENT (DEPT_LOCATION);
-
--- Transaction table Index
-CREATE INDEX IDX_TRANSACTION_TABLE_TIMESTAMP ON TRANSACTION_TABLE (TIME_STAMP);
-
--- Employee Table Index
-CREATE INDEX idx_employee_email ON EMPLOYEE (EMAIL_ID);
+-- Accounts Table Index
+CREATE INDEX idx_account_created_date ON ACCOUNTS (CREATED_DATE);
 
 -- Branch Table Indexes
 CREATE INDEX idx_branch_name ON BRANCH (BRANCH_NAME);
 CREATE INDEX idx_branch_city ON BRANCH (CITY);
 
--- Transaction Type Table Index
-CREATE INDEX idx_transaction_type_tran_type ON TRANSACTION_TYPE (TRANSACTION_TYPE);
+-- Customer Table Index
+CREATE INDEX idx_customer_login_username ON CUSTOMER (LOGIN);
+
+-- Employee Table Index
+CREATE INDEX idx_employee_login ON EMPLOYEE (LOGIN);
 
 -- Loan Type Table Index
 CREATE INDEX idx_loan_type_loan_type ON LOAN_TYPE (LOAN_TYPE);
 
+-- Transaction table Index
+CREATE INDEX IDX_TRANSACTION_TABLE_STATUS ON TRANSACTION_TABLE (STATUS);
